3f35f6f06eddf08f10def0f504ca55a50a9ac86a
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-12 02:02:02 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -9,29 +9,29 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public-a" {
   vpc_id                  = aws_vpc.main.id
-  availability_zone       = "us-east-2a"
+  availability_zone       = "ap-northeast-1a"
   cidr_block              = "10.0.0.0/24"
-  map_public_ip_on_launch = true       #サブネットで起動したインスタンスにパブリックIPを許可する
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-a"
   }
 }
 
-resource "aws_subnet" "public-b" {
+resource "aws_subnet" "public-c" {
   vpc_id                  = aws_vpc.main.id
-  availability_zone       = "us-east-2b"
+  availability_zone       = "ap-northeast-1c"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-b"
+    Name = "public-c"
   }
 }
 
 resource "aws_subnet" "private-a" {
   vpc_id            = aws_vpc.main.id
-  availability_zone = "us-east-2a"
+  availability_zone = "ap-northeast-1a"
   cidr_block        = "10.0.2.0/24"
 
   tags = {
@@ -39,13 +39,13 @@ resource "aws_subnet" "private-a" {
   }
 }
 
-resource "aws_subnet" "private-b" {
+resource "aws_subnet" "private-c" {
   vpc_id            = aws_vpc.main.id
-  availability_zone = "us-east-2b"
+  availability_zone = "ap-northeast-1c"
   cidr_block        = "10.0.3.0/24"
 
   tags = {
-    Name = "private-b"
+    Name = "private-c"
   }
 }
 resource "aws_internet_gateway" "gw" {
@@ -82,8 +82,8 @@ resource "aws_route_table_association" "public-a" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public-b" {
-  subnet_id      = aws_subnet.public-b.id
+resource "aws_route_table_association" "public-c" {
+  subnet_id      = aws_subnet.public-c.id
   route_table_id = aws_route_table.public.id
 }
 
@@ -92,7 +92,7 @@ resource "aws_route_table_association" "private-a" {
   route_table_id = aws_route_table.private.id
 }
 
-resource "aws_route_table_association" "private-b" {
-  subnet_id      = aws_subnet.private-b.id
+resource "aws_route_table_association" "private-c" {
+  subnet_id      = aws_subnet.private-c.id
   route_table_id = aws_route_table.private.id
 }
