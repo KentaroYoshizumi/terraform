6a61a978ea77f67c63dae5bebf747bfd455c449a
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-12 01:39:49 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,12 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "aws-terraform-vpc"
+  }
+}
+
 resource "aws_subnet" "public-a" {
   vpc_id                  = aws_vpc.main.id
   availability_zone       = "us-east-2a"
