ff9847a1dcdab7d901cc21150900c4c658b32b60
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-12 21:58:34 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "hello-world" {
     key_name      = "aws-terraform"
     instance_type = "t2.micro"
 
+    subnet_id              = aws_subnet.public-a.id
+    vpc_security_group_ids = ["${aws_security_group.allow.id}"]
+
     tags = {
         Name = "HelloWorld"
     }
